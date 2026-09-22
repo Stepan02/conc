3c284fdc3893291b237927e7f02d02a4406812ea
Stepan02 <85454063+Stepan02@users.noreply.github.com>
2026-09-22 23:22:34 +0200

    feat: emulate sysinfo syscall

--- sandbox/security.c
+++ sandbox/security.c
@@ -2,8 +2,91 @@
 #include <linux/seccomp.h>
 #include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/prctl.h>
 #include <sys/ioctl.h>
+#include <sys/sysinfo.h>
+
+struct resources {
+    uint64_t max;
+    uint64_t current;
+};
+
+static struct resources read_resources(pid_t pid) {
+    struct resources response = {0, 0};
+
+    char full_path[256];
+    snprintf(full_path, 256, "/proc/%d/cgroup", pid);
+
+    FILE *file = fopen(full_path, "r");
+    if (!file) {
+        return response;
+    }
+
+    // parse individual cgroup lines
+    char line[256];
+    if (fgets(line, sizeof(line), file)) {
+        char *cgroup_line = strchr(line, ':');
+        if (cgroup_line && cgroup_line[1] == ':') {
+            cgroup_line += 2; // skip ::
+            cgroup_line[strcspn(cgroup_line, "\r\n")] = 0; // cut new line
+
+            char base_path[256];
+            snprintf(base_path, sizeof(base_path), "/sys/fs/cgroup%s", cgroup_line);
+            fclose(file);
+
+            char buffer[64];
+
+            // read memory max
+            char memory_max_path[320];
+            snprintf(memory_max_path, sizeof(memory_max_path), "%s/memory.max", base_path);
+            FILE *file_max = fopen(memory_max_path, "r");
+            if (file_max) {
+                if (fgets(buffer, sizeof(buffer), file_max) && strncmp(buffer, "max", 3) != 0) {
+                    response.max = (uint64_t)strtoull(buffer, NULL, 10);
+                }
+                fclose(file_max);
+            }
+
+            // read memory current
+            char memory_current_path[320];
+            snprintf(memory_current_path, sizeof(memory_current_path), "%s/memory.current", base_path);
+            FILE *file_current = fopen(memory_current_path, "r");
+            if (file_current) {
+                if (fgets(buffer, sizeof(buffer), file_current) && strncmp(buffer, "max", 3) != 0) {
+                    response.current = (uint64_t)strtoull(buffer, NULL, 10);
+                }
+                fclose(file_current);
+            }
+
+            return response;
+        }
+    }
+
+    fclose(file);
+    return response;
+}
+
+static int memory_write(pid_t pid, unsigned long remote_address, void *buffer, size_t length) {
+    char memory_path[64];
+    snprintf(memory_path, sizeof(memory_path), "/proc/%d/mem", pid);
+
+    int fd = open(memory_path, O_WRONLY);
+    if (fd < 0) {
+        return -1;
+    }
+
+    if (pwrite(fd, buffer, length, (off_t)remote_address) != (ssize_t)length) {
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+    return 0;
+}
 
 int setup_syscall_blacklist(void) {
     scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_ALLOW);
@@ -32,6 +115,7 @@ int setup_syscall_blacklist(void) {
         SCMP_SYS(umount2),
         SCMP_SYS(ustat),
         SCMP_SYS(sysfs),
+        SCMP_SYS(sysinfo),
         SCMP_SYS(acct),
         SCMP_SYS(syslog),
         SCMP_SYS(add_key),
@@ -87,6 +171,52 @@ static struct seccomp_notif_resp syscall_emulator(struct seccomp_notif *request)
     // setup response
     struct seccomp_notif_resp response = {};
     response.id = request->id;
+
+    // emulate sysinfo syscall
+    if (request->data.nr == SCMP_SYS(sysinfo)) {
+        unsigned long remote_address = request->data.args[0];
+
+        // get sysinfo from host
+        struct sysinfo info = {};
+        if (sysinfo(&info) < 0) {
+            response.error = -errno;
+            return response;
+        }
+
+        // get cgroups info
+        struct resources memory = read_resources(request->pid);
+
+        // set cgroup values if set
+        if (memory.max > 0) {
+            info.mem_unit = 1;
+            info.totalram = memory.max;
+
+            // swap is disabled
+            info.totalswap = 0;
+            info.freeswap = 0;
+
+            // resolve free ram
+            if (memory.current < memory.max) {
+                info.freeram = memory.max - memory.current;
+            } else {
+                info.freeram = 0;
+            }
+
+            info.bufferram = 0;
+            info.sharedram = 0;
+        }
+
+        // write sysinfo to child
+        if (memory_write(request->pid, remote_address, &info, sizeof(info)) < 0) {
+            response.error = -EFAULT;
+        } else {
+            response.error = 0;
+            response.val = 0;
+        }
+
+        return response;
+    }
+
     response.error = -EPERM; // permission denied error
     response.val = 0;
 
